071a1d08958096e8bb0093217702ee0c6d9d1ad4
albertsuzx <37587199+albertsuzx@users.noreply.github.com>
2019-08-09 17:27:32 +0200

    Update Fotografiska.py

--- Basic/Fotografiska.py
+++ Basic/Fotografiska.py
@@ -1,26 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 url = 'https://fotografiska.com/sto/en/experience/exhibitions/'
-r = requests.get(url)
-print(r)
-r.encoding = 'utf-8'
+html = requests.get(url)
+print(html)
+html.encoding = 'utf-8'
 
-soup = BeautifulSoup(r.text,'lxml')
-for a in soup.find_all('a'):
-    link = a['href']
-    if (re.match(r'^(.*)/exhibition/latent', link)):
-        # print(link)
-        exhibition = requests.get(link)
-        exhibition.encoding = 'utf-8'
-        soupExhibition = BeautifulSoup(exhibition.content,'lxml')
-        # print(soupExhibition.prettify())
+soup = BeautifulSoup(html.text,'lxml')
 
-        # print(soupExhibition.title.string)
-        for hero in soupExhibition.find_all(class_ = "hero__content"):
-            print(hero)
-            print(hero.find_all(class_ = "container"))
-            print(hero.find_all(class_ = "small"))
+comments = []
 
-print()
+links = []
+
+for link in soup.findAll('a', attrs={'href': re.compile(r"^(.*)/exhibition/latent")}):
+    links.append(link.get('href'))
+
+# dedup the list of url containing diff. exhibitions
+links = list(set(links))
+# print(links)
+
+for link in links:
+    comment = {}
+
+    # print(link)
+    exhibition = requests.get(link)
+    exhibition.encoding = 'utf-8'
+    soupExhibition = BeautifulSoup(exhibition.content,'lxml')
+    # print(soupExhibition.prettify())
+
+    soupHead = soupExhibition.find(class_ = 'hero__content')
+
+    comment['artist_name'] = soupHead.h2.string
+    comment['exhibition_name'] = soupHead.h1.string
+    comment['date'] = " ".join(soupHead.p.string.split())
+    # comment['url'] = link
+
+    # comment['exhibition_name'] = soupHead.find('i', attrs={'class': 'hero__content'}).text.strip()
+
+    comments.append(comment)
+
+    # print(soupExhibition.title.string)
+    # for hero in soupExhibition.find_all(class_ = "hero__content"):
+    #    print(hero)
+    #    print(hero.find_all(class_ = "container"))
+    #    print(hero.find_all(class_ = "small"))
+
+df = pd.DataFrame(comments)
+print(df)
+df.to_csv('foto.csv', encoding='utf-8')
